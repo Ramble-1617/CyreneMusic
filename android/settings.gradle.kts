88498c54da02b7ba07cdf801245249f8a11545df
Ramble-1617 <gulutuantuan1617@gmail.com>
2026-09-22 18:05:10 +0800

    ci: upgrade Android Gradle Plugin for Flutter

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
-        
+
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -24,7 +24,7 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
